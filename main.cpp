83d63bcd59cda2d63afad31f79725ea9e248e3ea
nooreyad <nooreyadd39@gmail.com>
2023-12-01 14:12:40 +0200

    setting up the project

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    // Construct a suffix tree containing all suffixes of "bananabanaba$"
+
+    //              0123456789012
+    //SuffixTree t("bananabanaba$");
+
+    //t.Search("ana"); // Prints: 1 3 7
+    //t.Search("naba"); // Prints: 4 8
+
+    // Add test cases here.
+
+    return 0;
+}
